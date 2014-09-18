b42ebeafa5888e0eda47fd4e1c65859662cee323
Gordon Lesti <info@gordonlesti.com>
2014-09-18 16:23:27 +0200

    test via grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,24 @@
 module.exports = function(grunt) {
-    // Do grunt-related things in here
+    "use strict";
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            grunt: "Gruntfile.js",
+            source: "src/**/*.js"
+        },
+        jscs: {
+            src: "src/*.js",
+            options: {
+                config: ".jscsrc"
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-jscs");
+
+    grunt.registerTask("default", ["jshint", "jscs"]);
 };
