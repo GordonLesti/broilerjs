e94e42d51f828b52a1042b89dcf01d2fdbc6cd04
Gordon Lesti <info@gordonlesti.com>
2014-09-18 18:16:52 +0200

    add jsonlint and uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,32 @@ module.exports = function(grunt) {
         },
         jscs: {
             src: "src/*.js",
+            gruntfile: "Gruntfile.js",
             options: {
                 config: ".jscsrc"
+            }
+        },
+        jsonlint: {
+            pkg: {
+                src: [ "package.json" ]
+            }
+        },
+        uglify: {
+            options: {
+                banner: "/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.license %> */\n"
+            },
+            build: {
+                src: "src/broiler.js",
+                dest: "build/broiler-<%= pkg.version %>.min.js"
             }
         }
     });
 
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-jscs");
+    grunt.loadNpmTasks("grunt-jsonlint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
 
-    grunt.registerTask("default", ["jshint", "jscs"]);
+    grunt.registerTask("default", [ "jshint", "jscs", "jsonlint", "uglify" ]);
+    grunt.registerTask("ci", [ "jshint", "jscs", "jsonlint" ]);
 };
